dd76e39bd3b76187774b04871b64e6691c2260cd
InGyu Hwang <hwanginkyu42@naver.com>
2022-12-26 22:22:28 +0900

    MVC(미완)

---
+++ #1MyMVC/oop-project/src/main/java/org/example/User.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class User {
+    private String password;
+
+    public void initPassword(){
+        RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+    }
+}
