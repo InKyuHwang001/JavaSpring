9355d7c2b20368aa5b38df7fd86c5130da794838
InGyu Hwang <hwanginkyu42@naver.com>
2023-04-19 22:28:23 +0900

    Validation

--- springMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ springMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -43,15 +45,14 @@ public class ValidationItemControllerV1 {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
 
-        //검증 오류 결과 보관
+        //검증 오류 결과를 보관
         Map<String, String> errors = new HashMap<>();
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             errors.put("itemName", "상품 이름은 필수입니다.");
         }
-        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >
-                1000000) {
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
@@ -68,6 +69,7 @@ public class ValidationItemControllerV1 {
 
         //검증에 실패하면 다시 입력 폼으로
         if (!errors.isEmpty()) {
+            log.info("errors = {} ", errors);
             model.addAttribute("errors", errors);
             return "validation/v1/addForm";
         }
